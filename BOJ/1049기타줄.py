29c0d43d05da97387096cf88cb3ae3fa5fdab4f9
thegr8od <zzjoon97@naver.com>
2024-01-26 15:35:28 +0900

    240126

--- BOJ/1049기타줄.py
+++ BOJ/1049기타줄.py
@@ -1,23 +1,26 @@
-n,m = map(int,(input().split()))
-lst = []
-for i in range(m):
-    lst.append(list(map(int, input().split())))
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split(' '))
+
+minPack = 1001 # 주어지는 입력보다 더 큰 값으로 초기화
+minSingle = 1001
+for i in range(M):
+  p, s = map(int, input().split(' '))
+  minPack = min(minPack, p)
+  minSingle = min(minSingle, s)
+
+result = 0
 
 
-min6 = 2000
-min1 = 2000
-
-for i in lst:
-    if i[0] <= min6:
-        min6 = i[0]
-    if i[1] <= min1:
-        min1 = i[1]
-
-sum = 0
-if min6 < min1 * 6:
-    sum += min6 * (n / 6)
-    sum += min1 * (n % 6)
+if minPack > minSingle*6:
+  result += N * minSingle
 else:
-    sum += min1 * n
+  result += (N//6) * minPack
+  if (N%6)*minSingle > minPack:
+    result += minPack
+  else:
+    result+= (N%6)*minSingle
 
-print(sum)
+print(result)
---
+++ BOJ/4963섬의개수.py
@@ -0,0 +1,33 @@
+
+def dfs(x,y):
+    if x <= -1 or x >=h or y<= -1 or y >=w:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        dfs(x-1,y)
+        dfs(x-1,y+1)
+        dfs(x,y+1)
+        dfs(x+1, y+1)
+        dfs(x+1,y)
+        dfs(x+1, y-1)
+        dfs(x,y-1)
+        dfs(x-1,y-1)
+        return True
+    return False
+
+while True:
+    w, h = map(int, input().split())
+    if w ==0 and h ==0:
+        break
+    else:
+        graph = []
+        for i in range(h):
+            graph.append(list(map(int, input().split())))
+
+        result = 0
+        for i in range(h):
+            for j in range(w):
+                if dfs(i,j) == True:
+                    result += 1
+
+        print(result)
