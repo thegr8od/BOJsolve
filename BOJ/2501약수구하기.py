6ee7182d4f327999f1849edeb2ef285a973570b0
thegr8od <zzjoon97@naver.com>
2024-02-13 16:38:42 +0900

    240213

--- BOJ/2501약수구하기.py
+++ BOJ/2501약수구하기.py
@@ -1,11 +1,12 @@
 n, k = map(int,input().split())
 
 data = []
+
 for i in range(1,n+1):
     if n % i == 0:
         data.append(i)
 
-if len(data) < k+1:
+if len(data) < k:
     print(0)
 else:
     print(data[k-1])
