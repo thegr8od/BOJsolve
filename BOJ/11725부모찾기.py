56c52aa68c292b755e5cebc335adcbfd4c63689e
thegr8od <zzjoon97@naver.com>
2024-02-14 14:09:30 +0900

    240214

--- BOJ/11725부모찾기.py
+++ BOJ/11725부모찾기.py
@@ -4,21 +4,19 @@ input = sys.stdin.readline
 
 n = int(input())
 
-graph = [[] for i in range(n+1)]
+graph = [[] for _ in range(n+1)]
 
 for i in range(n-1):
-    a, b = map(int, input().split())
-    graph[a].append[b]
-    graph[b].append[a]
+    a,b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
 visited = [0] * (n+1)
-
-arr = []
 
 def dfs(s):
     for i in graph[s]:
         if visited[i] == 0:
-            visited[i] = s
+            visited[i] =s
             dfs(i)
 
 dfs(1)
---
+++ BOJ/9372상근이의여행.py
@@ -0,0 +1,7 @@
+t = int(input())
+
+for i in range(t):
+    n,m = map(int, input().split())
+    for j in range(m):
+        a,b = map(int, input().split())
+    print(n-1)
