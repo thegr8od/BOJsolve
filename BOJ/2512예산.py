5b84241421827147ec97cf3f2ac4e81b81456724
thegr8od <zzjoon97@naver.com>
2024-02-16 14:40:56 +0900

    240216

---
+++ BOJ/2512예산.py
@@ -0,0 +1,24 @@
+def binary_search(array, target, start, end):
+    result = 0
+    while start<=end:
+        total = 0
+        mid = (start + end) // 2
+        for x in array:
+            if x <= mid:
+                total += x
+            else:
+                total += mid
+        
+        if total > target:
+            end = mid - 1
+        else:
+            result = mid
+            start = mid + 1
+    return result
+
+n = int(input())
+data = list(map(int, input().split()))
+total = int(input())
+data.sort()
+print(binary_search(data, total, data[0], data[-1]))
+
