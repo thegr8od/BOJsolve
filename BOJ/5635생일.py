d9858da1b03d422d8de558e0de54b05575a89875
thegr8od <zzjoon97@naver.com>
2024-02-21 17:48:29 +0900

    240221

---
+++ BOJ/5635생일.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+data = []
+for i in range(5):
+    data.append(list(input().split()))
+
+data.sort(key = lambda x: (int(x[-1]),int(x[-2]),int(x[-3])))
+
+print(data[-1][0])
+print(data[0][0])
