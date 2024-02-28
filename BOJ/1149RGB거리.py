21a9fa322ffa61d9257ef6c1b66328af48f0db83
thegr8od <zzjoon97@naver.com>
2024-02-28 16:27:38 +0900

    240228

---
+++ BOJ/1149RGB거리.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+d = [[0] * 3 for i in range(n)]
+print(d)
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().split())))
+
+print(data)
+
+d[0][0], d[0][1], d[0][2] = data[0][0],data[0][1],data[0][2]
+
+for i in range(1,n):
+    d[i][0] = min(d[i-1][1] + data[i][0], d[i-1][2] + data[i][0])
+    d[i][1] = min(d[i-1][0] + data[i][1], d[i-1][2] + data[i][1])
+    d[i][2] = min(d[i-1][0] + data[i][2], d[i-1][1] + data[i][2])
+
+
+print(min(d[n-1][0],d[n-1][1],d[n-1][2]))
