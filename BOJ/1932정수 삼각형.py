ce2d082e8853670717eb49085da2e24e6a77deeb
thegr8od <zzjoon97@naver.com>
2024-02-29 17:12:33 +0900

    240229

---
+++ BOJ/1932정수 삼각형.py
@@ -0,0 +1,19 @@
+
+d = []
+n = int(input())
+for _ in range(n):
+    d.append(list(map(int, input().split())))
+
+
+for i in range(1,n):
+    for j in range(i):
+        if j==0:
+            d[i][j] += d[i-1][j]
+        elif j==i:
+            d[i][j] += d[i-1][j-1]
+        else:
+            d[i][j] += max(d[i-1][j-1], d[i-1][j])
+
+print(max(d[n-1]))
+
+
