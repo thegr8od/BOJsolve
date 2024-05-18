ce14bc32fd9d8c1f323bdb77b061c1cc855276c5
thegr8od <zzjoon97@naver.com>
2024-05-18 17:42:06 +0900

    240518

--- BOJ/11725트리의 부모 찾기2.py
+++ BOJ/11725트리의 부모 찾기2.py
@@ -1,3 +1,5 @@
+import sys
+sys.setrecursionlimit(10**6)
 n = int(input())
 
 graph = [[] for _ in range(n+1)]
@@ -7,5 +9,16 @@ for _ in range(n-1):
     graph[a].append(b)
     graph[b].append(a)
     
-for i in range(2,n+1):
-    print(graph[i][0])
+visited = [0] * (n+1)
+
+def dfs(x):
+    for i in graph[x]:
+        if visited[i] == 0:
+            visited[i] = x
+            dfs(i)
+
+dfs(1)
+
+for j in range(2,n+1):
+    print(visited[j])
+    
---
+++ BOJ/1476날짜계산.py
@@ -0,0 +1,20 @@
+e,s,m = map(int,input().split())
+
+a,b,c = 1,1,1
+cnt = 1
+while True:
+    if e == a and s == b and m == c:
+        break
+    else: 
+        cnt += 1
+        a += 1
+        if a == 16:
+            a = 1
+        b += 1
+        if b == 29:
+            b = 1
+        c += 1
+        if c == 20:
+            c = 1
+    
+print(cnt)
